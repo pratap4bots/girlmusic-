886d0501c291155902f5ee77e4cf82d5b42a425c
Venom Pratap <pratapbots@gmail.com>
2026-06-30 20:01:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ API_KEY = getenv("API_KEY", None) # youtube song api key, generate free key or b
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/pratap4bots/PagliMusic",
+    "https://github.com/pratap4bots/girlmusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
@@ -43,7 +43,7 @@ GIT_TOKEN = getenv(
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/venomvipbots") 
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/venompratapchat")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/+hFO7JXtvapVlZGM1")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
